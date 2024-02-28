492d4686d62391398e250f3cb2f11dce16281aa2
SymuNadeem <msymunnadeem@gmail.com>
2024-02-28 23:40:38 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className=' text-red-500'>
+            Hello Nur IT
+      </div>
+    </>
+  )
+}
+
+export default App
