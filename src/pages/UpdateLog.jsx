e21a48bcf635c79d622d274f4b40aae27bef1a80
SymuNadeem <msymunnadeem@gmail.com>
2024-03-05 23:48:17 +0600

    update page set

--- src/pages/UpdateLog.jsx
+++ src/pages/UpdateLog.jsx
@@ -1,10 +1,39 @@
-
+import { Link } from "react-router-dom";
+import FooterAllPage from "../components/shared/Footer/FooterAllPage";
+import TitleHeading from "../components/shared/TitleHeading";
+import { updateAndLog } from "../data/updateAndLog";
 
 const UpdateLog = () => {
   return (
-    <div>
-      UpdateLog
-    </div>
+    <>
+      <TitleHeading
+        emoji="☑️"
+        description="Update logs of TailAdmin"
+        title="Update Logs"
+      />
+      <div className="w-[828px]">
+        <div className="container_fluid">
+          <div className="mt-20 pb-25 flex flex-wrap gap-4">
+            {updateAndLog.map((language) => (
+              <div
+                key={language.id}
+                className="w-[217px] h-[122px] border p-4 footer_bar duration-200"
+              >
+                <Link to={language.link} className="hover:text-headingText hover:underline-offset-4 hover:underline duration-200  text-blueActive font-bold focus:text-blueActive">{language.title}</Link>
+              </div>
+            ))}
+          </div>
+          <div className="pb-10">
+            <FooterAllPage
+              title1="Installation"
+              title2="HTML Components"
+              link1="/installation"
+              link2="/components/html-components"
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
