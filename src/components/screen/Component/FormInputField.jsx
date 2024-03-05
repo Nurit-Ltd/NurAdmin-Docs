e807ba7736ad8ba1a6205536b26a5409cfda739b
SymuNadeem <msymunnadeem@gmail.com>
2024-03-05 12:39:53 +0600

    markdown setup problem

--- src/components/screen/Component/FormInputField.jsx
+++ src/components/screen/Component/FormInputField.jsx
@@ -1,23 +1,47 @@
+import Markdown from "markdown-to-jsx";
+import { useEffect, useState } from "react";
 import { htmlComponents } from "../../../data/formInputData";
-
 const FormInputField = () => {
+  
+  const [postContent, setPostContent] = useState("");
+
+  useEffect(() => {
+    import("../../../markdown/artical.md")
+      .then(res => {
+        fetch(res.default)
+          .then(response => response.text())
+          .then(response => setPostContent(response))
+          .catch(err => console.log(err))
+      })
+  }, []); 
   return (
     <div className="pt-10">
-       {htmlComponents.map((item)=>(
-         <div key={item.id} className="space-y-8">
-             <h3 className="text-xl text-headingText font-bold leading-6">{item.title}</h3>
-             <p className="text-headingText leading-6">{item.description.split(' ').map((word, index) => (
-                 (word === 'Default' || word === 'Active' || word === 'Disabled') ? 
-                 <span className="font-bold" key={index}>{word} </span> : word + ' '
-               ))}</p>
-             <div>
-               <img src={item.formImg} alt="form-input" />
-             </div>
-         </div>
-       ))}
-       <div className="w-full h">
-          <p></p>
-       </div>
+      {htmlComponents.map((item) => (
+        <div key={item.id} className="space-y-8">
+          <h3 className="text-xl text-headingText font-bold leading-6">
+            {item.title}
+          </h3>
+          <p className="text-headingText leading-6">
+            {item.description.split(" ").map((word, index) =>
+              word === "Default" || word === "Active" || word === "Disabled" ? (
+                <span className="font-bold" key={index}>
+                  {word}{" "}
+                </span>
+              ) : (
+                word + " "
+              )
+            )}
+          </p>
+          <div>
+            <img src={item.formImg} alt="form-input" />
+          </div>
+        </div>
+      ))}
+      <div className="w-full h">
+      <Markdown>
+            {postContent}
+          </Markdown>
+      </div>
     </div>
   );
 };
