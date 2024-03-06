e2d3886247005f3561aef6e123532efc111f6e51
SymuNadeem <msymunnadeem@gmail.com>
2024-03-06 10:24:53 +0600

    version data make

---
+++ src/data/updateHtml.js
@@ -0,0 +1,109 @@
+
+export const updateHtml = {
+  version: "1.3.0",
+  releaseDate: ["Jan", 2024],
+  newFeatures: [
+    {
+      id: 1,
+      name: "Feature 01",
+      details: "New Dashboard variant Stocks with a DataStats slider, One Chart graph, My Stocks, Trending Stocks, and Latest Transaction Cards."
+    },
+    {
+      id: 2,
+      name: "Feature 02",
+      details: "Pro Form Elements Range Slider & File Drop Zone."
+    },
+    {
+      id: 3,
+      name: "Feature 03",
+      details: "Pro Form Layout Contact Form 2 & Survey Form."
+    },
+    {
+      id: 4,
+      name: "Feature 04",
+      details: "Pro Tables."
+    },
+    {
+      id: 5,
+      name: "Feature 05",
+      details: "Invoice Two."
+    },
+    {
+      id: 6,
+      name: "Feature 06",
+      details: "FAQs."
+    },
+    {
+      id: 7,
+      name: "Feature 07",
+      details: "Teams One & Two."
+    },
+    {
+      id: 8,
+      name: "Feature 08",
+      details: "Terms & Conditions."
+    },
+    {
+      id: 9,
+      name: "Feature 09",
+      details: "Avatars One & Two."
+    },
+    {
+      id: 10,
+      name: "Feature 10",
+      details: "List One, Two & Three."
+    },
+    {
+      id: 11,
+      name: "Feature 11",
+      details: "Notifications Four."
+    },
+    {
+      id: 12,
+      name: "Feature 12",
+      details: "Spinners One & Two."
+    },
+    {
+      id: 13,
+      name: "Feature 13",
+      details: "Coming Soon."
+    },
+    {
+      id: 14,
+      name: "Feature 14",
+      details: "2-Step Verification."
+    },
+    {
+      id: 15,
+      name: "Feature 15",
+      details: "Under Maintenance."
+    }
+  ]
+  enhancements: [
+    {
+      id: 2,
+      name: "Enhancement 02",
+      details: "Integrate flatpickr in [Date Picker/Form Elements]."
+    },
+    {
+      id: 3,
+      name: "Enhancement 03",
+      details: "Change color after selecting an option [Select Element/Form Elements]."
+    },
+    {
+      id: 4,
+      name: "Enhancement 04",
+      details: "Make it functional [Multiselect Dropdown/Form Elements]."
+    },
+    {
+      id: 5,
+      name: "Enhancement 05",
+      details: "Make the best value editable [Pricing Table One/Pricing Table]."
+    },
+    {
+      id: 6,
+      name: "Enhancement 06",
+      details: "Add Default Layout Component and make App/Layout cleaner and use it on every page."
+    }
+  ]
+};
