f395322fe891fdaf29785d179fe45fa1aab1782a
SymuNadeem <msymunnadeem@gmail.com>
2024-03-07 17:41:34 +0600

    react all page code

--- src/components/screen/Component/ReactCom/BreadCum.jsx
+++ src/components/screen/Component/ReactCom/BreadCum.jsx
@@ -1,7 +1,35 @@
+import ReactMarkdown from "react-markdown";
 import { reactBreadCrumb } from "../../../../data/reactComData";
-
+import CopyToClip from "../../../shared/CopyToClip";
 
 const BreadCum = () => {
+  const markdown = `
+  \`\`\`
+  import React from 'react';
+  import { Link } from 'react-router-dom';
+
+  const Breadcrumb = (props) => {
+  return (
+  <div className='mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between'>
+    <h2 className='text-title-md2 font-semibold text-black dark:text-white'>
+      {props.pageName}
+    </h2>
+
+    <nav>
+      <ol className='flex items-center gap-2'>
+        <li>
+          <Link to='/'>Dashboard /</Link>
+        </li>
+        <li className='text-primary'>{props.pageName}</li>
+      </ol>
+    </nav>
+  </div>
+  )
+  }
+
+  export default Breadcrumb;
+  \`\`\`
+  `;
   return (
     <div className="pt-20">
     {reactBreadCrumb.map((item) => (
@@ -18,6 +46,12 @@ const BreadCum = () => {
           <p className="text-headingText leading-6">{item.markText}</p>
         </div>
       ))}
+       <div className="mt-8 w-full relative">
+        <div className="group  px-4 py-2 bg-bgBluer overflow-x-auto">
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+          <CopyToClip markdown={markdown} />
+        </div>
+      </div>
     </div>
   );
 };
