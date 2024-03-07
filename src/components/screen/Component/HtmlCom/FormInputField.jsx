199fb6cf83811a66c932618a14a6d0d72e389f85
SymuNadeem <msymunnadeem@gmail.com>
2024-03-07 12:18:12 +0600

    markdown code

--- src/components/screen/Component/HtmlCom/FormInputField.jsx
+++ src/components/screen/Component/HtmlCom/FormInputField.jsx
@@ -1,20 +1,35 @@
-import Markdown from "markdown-to-jsx";
-import { useEffect, useState } from "react";
+import ReactMarkdown from 'react-markdown';
 import { htmlComponents } from "../../../../data/formInputData";
 const FormInputField = () => {
+ 
+  const markdown = `
+  \`\`\`html
+  <div>
+    <label class="mb-3 block text-black dark:text-white">
+      Default Input
+    </label>
+    <input type="text" placeholder="Default Input"
+      class="border-stroke dark:border-form-strokedark dark:bg-form-input focus:border-primary dark:focus:border-primary active:border-primary bg-transparent w-full rounded-lg border-[1.5px] py-3 px-5 font-medium outline-none transition disabled:cursor-default disabled:bg-whiter">
+  </div>
   
-  const [postContent, setPostContent] = useState("");
-  const fileName ='artical.md'
+  <div>
+    <label class="mb-3 block text-black dark:text-white">
+      Active Input
+    </label>
+    <input type="text" placeholder="Active Input"
+      class="border-primary focus:border-primary active:border-primary dark:bg-form-input bg-transparent w-full rounded-lg border-[1.5px] py-3 px-5 font-medium outline-none transition disabled:cursor-default disabled:bg-whiter" />
+  </div>
+  
+  <div>
+    <label class="mb-3 block font-medium text-black dark:text-white">
+      Disabled label
+    </label>
+    <input type="text" placeholder="Disabled label" disabled=""
+      class="border-stroke dark:border-form-strokedark dark:bg-form-input focus:border-primary dark:focus:border-primary active:border-primary bg-transparent w-full rounded-lg border-[1.5px] py-3 px-5 font-medium outline-none transition disabled:cursor-default disabled:bg-whiter dark:disabled:bg-black" />
+  </div>
+  \`\`\`
+  `
 
-  useEffect(() => {
-    import(`../../../markdown/${fileName}`)
-      .then(res => {
-        fetch(res.default)
-          .then(response => response.text())
-          .then(response => setPostContent(response))
-          .catch(err => console.log(err))
-      })
-  }, []); 
   return (
     <div className="pt-20">
       {htmlComponents.map((item) => (
@@ -38,8 +53,8 @@ const FormInputField = () => {
           </div>
         </div>
       ))}
-      <div className="w-full border">
-      <Markdown>{postContent}</Markdown>
+      <div className="w-full border overflow-x-auto">
+      <ReactMarkdown>{markdown}</ReactMarkdown>
       </div>
     </div>
   );
