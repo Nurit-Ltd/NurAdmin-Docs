109ea2e08af5bffa794193520f0de9abed540bd5
SymuNadeem <msymunnadeem@gmail.com>
2024-03-07 15:22:58 +0600

    copy for code

--- src/components/screen/Component/HtmlCom/AnalyticsChart.jsx
+++ src/components/screen/Component/HtmlCom/AnalyticsChart.jsx
@@ -1,6 +1,48 @@
+import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { BsCopy } from "react-icons/bs";
+import ReactMarkdown from "react-markdown";
 import { analyticChart } from '../../../../data/formInputData';
 
+
 const AnalyticsChart = () => {
+  const [copied, setCopied] = useState(false);
+
+  const markdown = `
+  \`\`\`html
+  <div>
+    <label class="mb-3 block text-black dark:text-white">
+      Defaults Input
+    </label>
+    <input type="text" placeholder="Default Input"
+      class="border-stroke dark:border-form-strokedark dark:bg-form-input focus:border-primary dark:focus:border-primary active:border-primary bg-transparent w-full rounded-lg border-[1.5px] py-3 px-5 font-medium outline-none transition disabled:cursor-default disabled:bg-whiter">
+  </div>
+  
+  <div>
+    <label class="mb-3 block text-black dark:text-white">
+      Active Input
+    </label>
+    <input type="text" placeholder="Active Input"
+      class="border-primary focus:border-primary active:border-primary dark:bg-form-input bg-transparent w-full rounded-lg border-[1.5px] py-3 px-5 font-medium outline-none transition disabled:cursor-default disabled:bg-whiter" />
+  </div>
+  
+  <div>
+    <label class="mb-3 block font-medium text-black dark:text-white">
+      Disabled label
+    </label>
+    <input type="text" placeholder="Disabled label" disabled=""
+      class="border-stroke dark:border-form-strokedark dark:bg-form-input focus:border-primary dark:focus:border-primary active:border-primary bg-transparent w-full rounded-lg border-[1.5px] py-3 px-5 font-medium outline-none transition disabled:cursor-default disabled:bg-whiter dark:disabled:bg-black" />
+  </div>
+  \`\`\`
+  `;
+
+  const handleCopy = () => {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000); // Reset message after 2 seconds
+  };
+
   return (
     <div className="pt-20">
     {analyticChart.map((item)=>(
@@ -15,9 +57,23 @@ const AnalyticsChart = () => {
           </div>
       </div>
     ))}
-    <div className="w-full h">
-       <p></p>
-    </div>
+   <div className="w-full border px-4 py-2 bg-bgBluer overflow-x-auto">
+        <CopyToClipboard
+          text={markdown}
+          onCopy={handleCopy}
+          className="cursor-pointer rounded w-[25px] h-[25px] p-1 bg-white border items-center justify-center hover:flex duration-200"
+        >
+          <BsCopy className="text-grayMenu" />
+        </CopyToClipboard>
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </div>
+      {copied && (
+        <div className="w-[420px] h-[86px] border bg-white shadow-md fixed bottom-4 left-1/2 transform -translate-x-1/2">
+          <p className="text-green-600 ">
+          Copied to clipboard!
+        </p>
+        </div>
+      )}
  </div>
   );
 };
--- src/components/screen/Component/HtmlCom/FormInputField.jsx
+++ src/components/screen/Component/HtmlCom/FormInputField.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { BsCopy } from "react-icons/bs";
+import { RxCross2 } from "react-icons/rx";
 import ReactMarkdown from "react-markdown";
 import { htmlComponents } from "../../../../data/formInputData";
 
@@ -39,7 +40,7 @@ const FormInputField = () => {
     setCopied(true);
     setTimeout(() => {
       setCopied(false);
-    }, 2000); // Reset message after 2 seconds
+    }, 2000);
   };
 
   return (
@@ -65,21 +66,33 @@ const FormInputField = () => {
           </div>
         </div>
       ))}
-      <div className="w-full border px-4 py-2 bg-bgBluer overflow-x-auto">
-        <CopyToClipboard
-          text={markdown}
-          onCopy={handleCopy}
-          className="cursor-pointer rounded w-[25px] h-[25px] p-1 bg-white border items-center justify-center hover:flex duration-200"
-        >
-          <BsCopy className="text-grayMenu" />
-        </CopyToClipboard>
-        <ReactMarkdown>{markdown}</ReactMarkdown>
+      <div className="mt-8 w-full relative">
+        <div className="group  px-4 py-2 bg-bgBluer overflow-x-auto">
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+
+          <CopyToClipboard
+            text={markdown}
+            onCopy={handleCopy}
+            className="absolute invisible  group-hover:visible top-3 right-3 cursor-pointer rounded w-[25px] h-[25px] p-1 bg-white border items-center justify-center hover:flex duration-200"
+          >
+            <BsCopy className="text-gray-400 hover:text-grayMenu duration-150" />
+          </CopyToClipboard>
+        </div>
       </div>
       {copied && (
-        <div className="w-[420px] h-[86px] border bg-white shadow-md fixed bottom-4 left-1/2 transform -translate-x-1/2">
-          <p className="text-green-600 ">
-          Copied to clipboard!
-        </p>
+        <div className="w-[420px] h-[86px] p-4 border bg-white shadow-md fixed bottom-4 right-[-50px] transform -translate-x-1/2 flex items-center justify-between">
+          <div className="flex gap-4">
+            <BsCopy />
+            <div>
+              <h5 className="text-lg text-headingText font-medium">
+                Copied to clipboard
+              </h5>
+              <p className="text-grayPrimary">Paste it wherever you like.</p>
+            </div>
+          </div>
+          <div>
+           <RxCross2 onClick={()=>setCopied(false)} />
+          </div>
         </div>
       )}
     </div>
